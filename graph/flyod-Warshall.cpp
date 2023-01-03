673dd442d27295470e42bd8d5409f5c1f07197d7
HelloShiv <swagatswarooparida@gmail.com>
2023-01-03 15:11:56 +0530

    Floyd's Warshall - not for negative weight

--- graph/flyod-Warshall.cpp
+++ graph/flyod-Warshall.cpp
@@ -37,27 +37,27 @@ class Solution {
 };
 
 //{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n;
-		cin >> n;
-		vector<vector<int>>matrix(n, vector<int>(n, -1));
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < n; j++){
-				cin >> matrix[i][j];
-			}
-		}
-		Solution obj;
-		obj.shortest_distance(matrix);
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < n; j++){
-				cout << matrix[i][j] << " ";
-			}
-			cout << "\n";
-		}
-	}
-	return 0;
-}
+// int main(){
+// 	int tc;
+// 	cin >> tc;
+// 	while(tc--){
+// 		int n;
+// 		cin >> n;
+// 		vector<vector<int>>matrix(n, vector<int>(n, -1));
+// 		for(int i = 0; i < n; i++){
+// 			for(int j = 0; j < n; j++){
+// 				cin >> matrix[i][j];
+// 			}
+// 		}
+// 		Solution obj;
+// 		obj.shortest_distance(matrix);
+// 		for(int i = 0; i < n; i++){
+// 			for(int j = 0; j < n; j++){
+// 				cout << matrix[i][j] << " ";
+// 			}
+// 			cout << "\n";
+// 		}
+// 	}
+// 	return 0;
+// }
 // } Driver Code Ends
